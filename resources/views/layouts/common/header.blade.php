7f0f5e4bae6d44eec67257458911682432b6e28e
tarekmonjur <tarekmonjur@gmail.com>
2018-01-11 16:50:10 +0600

    header update

--- resources/views/layouts/common/header.blade.php
+++ resources/views/layouts/common/header.blade.php
@@ -13,11 +13,21 @@
                 <ul class="nav navbar-nav">
                     <li @if($menu == '') class="active" @endif ><a href="{{url('/')}}">Dashboard<span class="sr-only">(current)</span></a></li>
                     <li @if($menu == 'projects') class="active" @endif ><a href="{{url('/projects')}}">Projects</a></li>
+                    @if(canAccess("users"))
                     <li @if($menu == 'users') class="active" @endif ><a href="{{url('/users')}}">Users</a></li>
+                    @endif
+                    @if(canAccess("roles"))
                     <li @if($menu == 'roles') class="active" @endif ><a href="{{url('/roles')}}">Role</a></li>
+                    @endif
+                    @if(canAccess("teams"))
                     <li @if($menu == 'teams') class="active" @endif ><a href="{{url('/teams')}}">Teams</a></li>
+                    @endif
+                    @if(canAccess("company"))
                     <li @if($menu == 'company') class="active" @endif ><a href="{{url('/company')}}">Companies</a></li>
+                    @endif
+                    @if(canAccess("department"))
                     <li @if($menu == 'department') class="active" @endif ><a href="{{url('/department')}}">Department</a></li>
+                    @endif
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
